2239269b0f72d22802dfa71026bb62713d3c39da
Peyton <pekellogg@hotmail.com>
2022-05-01 17:47:16 -0700

    understood implicit/explicit rendering in Rails

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,8 @@
+class StaticController < ApplicationController
+    def about
+        # render "some_page"
+
+        # full path syntax (not preferable)
+        # render "static/some_page"
+    end
+end
